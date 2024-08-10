b52bba263d2b4f0fa370d972c2d7ff7027ed308f
sentator <sentator.dev@gmail.com>
2024-08-10 15:01:44 +0300

    add client-app

---
+++ apps/client-app/src/app/App.tsx
@@ -0,0 +1,14 @@
+import './App.scss';
+
+export function App() {
+  return (
+    <div>
+      <h1>
+        <span> Hello there, </span>
+        Welcome client-app 👋
+      </h1>
+    </div>
+  );
+}
+
+export default App;
